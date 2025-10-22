1297c1225d631b944151de81b9d06a34c09c4fc7
SRxMNz <edna.32limalima@gmail.com>
2025-10-21 22:16:52 -0300

    oou

--- projeto/include/cadastrar.php
+++ projeto/include/cadastrar.php
@@ -1,10 +1,18 @@
 <?php
-$primeiroNome = $_POST['primeiroNome'];
-$sobrenome = $_POST['sobrenome'];
-$escolhaPets = $_POST['escolhaPets'];
 
-echo("$primeiroNome -  $sobrenome - $escolhaPets <br/> Cadastrado com sucesso!!");
-echo('<br/> <a href="../index.php">Voltar</a>"')
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    
+    $primeiroNome = $_POST['primeiroNome'];
+    $sobrenome = $_POST['sobrenome'];
+    $escolhaPets = $_POST['escolhaPets'];
+    
+    echo("$primeiroNome -  $sobrenome - $escolhaPets <br/> Cadastrado com sucesso!!");
+    echo('<br/> <a href="../index.php">Voltar</a>"');
+    }
+else{
+    echo("get");
+}
 
 // header("location: ../index.php");
 
